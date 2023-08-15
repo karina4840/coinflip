b5c334d3fd65e99c592034ddd1eaa8286cd7a11d
karina ivanova <86211129+karina4840@users.noreply.github.com>
2023-08-15 13:21:07 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Join from "../components/Join";
+import CoinUpdates from "../components/CoinUpdates";
+import WhyUs from "../components/WhyUs";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <CoinUpdates />
+      <WhyUs />
+      <Join />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
