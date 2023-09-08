872f04e61c4f478b96b8554e3b6e753537e0ccd9
karina ivanova <86211129+karina4840@users.noreply.github.com>
2023-09-08 14:39:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const onRedirectCallback = (appState) => {
 
 const providerConfig = {
   domain: process.env.REACT_APP_AUTH0_DOMAIN,
-  clientId: process.env.REACT_APP_AUTH0_CLIENTID,
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
   onRedirectCallback,
   authorizationParams: {
     redirect_uri: window.location.origin,
